2abedf96859ecbf4c4974cedb217d3ed5accb072
nade <nade@if.de>
2023-10-19 15:15:31 +0200

    #2 implement keyboard - implement a direction enum for reciever side

--- communication/RF/reciever/reciever.ino
+++ communication/RF/reciever/reciever.ino
@@ -1,5 +1,3 @@
-// SimpleRx - the slave or the receiver
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -11,41 +9,57 @@ const byte thisSlaveAddress[22] = "ah8Bgq2UQja5uv3Ex6hJ6q";
 
 RF24 radio(CE_PIN, CSN_PIN);
 
-char dataReceived[10]; // this must match dataToSend in the TX
+enum Direction { FORWARD , LEFT, BACKWARD, RIGHT };
+
+unsigned short direction;
 bool newData = false;
 
-//===========
-
 void setup() {
-    Serial.begin(9600);
-
-    Serial.println("SimpleRx Starting");
     radio.begin();
     radio.setDataRate( RF24_250KBPS );
     radio.openReadingPipe(1, thisSlaveAddress);
     radio.startListening();
-}
 
-//=============
+    pinMode(4, OUTPUT);
+}
 
 void loop() {
     getData();
     showData();
 }
 
-//==============
-
 void getData() {
     if ( radio.available() ) {
-        radio.read( &dataReceived, sizeof(dataReceived) );
+        radio.read( &direction, sizeof(direction) );
         newData = true;
     }
 }
 
 void showData() {
     if (newData == true) {
-        Serial.print("Data received ");
-        Serial.println(dataReceived);
         newData = false;
+        drive();
     }
 }
+
+void drive() {
+  switch(direction) {
+    case FORWARD:
+      digitalWrite(4, LOW);
+      break;
+    case LEFT:
+      digitalWrite(4, HIGH);
+      break;
+    case BACKWARD:
+      digitalWrite(4, LOW);
+      break;
+    case RIGHT:
+      digitalWrite(4, HIGH);
+      break;
+    default:
+      break;
+  }
+}
+
+
+
