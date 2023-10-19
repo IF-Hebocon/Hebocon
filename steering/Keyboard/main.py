1d1d2fcc7e7ecc832881437d39ccc88797396dcd
Natalie <natalie.dessi@outlook.de>
2023-10-19 23:36:30 +0200

    #2 implement keyboard - add close mechanism - add some prints

--- steering/Keyboard/main.py
+++ steering/Keyboard/main.py
@@ -1,5 +1,5 @@
 from serial import Serial
-from pynput.keyboard import Listener
+from pynput.keyboard import Listener, Key
 from time import sleep
 
 siri = Serial(port='COM4', baudrate=9600, timeout=.1)
@@ -10,12 +10,19 @@ keys = { 'w': 1,
          'd': 4 }
 
 def on_press(key):
-        if hasattr(key, 'char') and key.char in keys.keys():
-            siri.write(bytes(str(keys[key.char]), 'utf-8'))
+        if key is Key.esc:
+            siri.close()
+            exit()
+        if hasattr(key, 'char'):
+            if key.char in keys.keys():
+                siri.write(bytes(str(keys[key.char]), 'utf-8'))
 
 def main():
     with Listener(on_press=on_press) as listener:
         listener.join()
+
 if __name__ == '__main__':
+    print("Programm startet...")
     sleep(2)
+    print("Programm gestartet! Drücke ESC zum beenden")
     main()
