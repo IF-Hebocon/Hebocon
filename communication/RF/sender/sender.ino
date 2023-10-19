b88cbfe2d5222d84f3797f9c4f54aa94c616e0ae
Natalie <natalie.dessi@outlook.de>
2023-10-19 15:37:56 +0200

    #2 implement keyboard - send serial recieved valid ints to reciever

--- communication/RF/sender/sender.ino
+++ communication/RF/sender/sender.ino
@@ -1,5 +1,3 @@
-// SimpleTx - the master or the transmitter
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
@@ -10,54 +8,43 @@
 
 const byte slaveAddress[22] = "ah8Bgq2UQja5uv3Ex6hJ6q";
 
+RF24 radio(CE_PIN, CSN_PIN);
 
-RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+enum Direction { FORWARD, LEFT, BACKWARD, RIGHT };
 
-char dataToSend[10] = "Message 0";
-char txNum = '0';
-
-
-unsigned long currentMillis;
-unsigned long prevMillis;
-unsigned long txIntervalMillis = 1000; // send once per second
-
+unsigned short direction;
 
 void setup() {
-
     Serial.begin(9600);
-
-    Serial.println("SimpleTx Starting");
 
     radio.begin();
     radio.setDataRate( RF24_250KBPS );
-    radio.setRetries(3,5); // delay, count
+    radio.setRetries(3, 5);
     radio.openWritingPipe(slaveAddress);
 }
 
 //====================
 
 void loop() {
-    currentMillis = millis();
-    if (currentMillis - prevMillis >= txIntervalMillis) {
+    if (Serial.available() && read()) {
         send();
-        prevMillis = millis();
     }
 }
 
-//====================
+bool read() {
+  direction = Serial.parseInt();
+  return direction != 0;
+}
 
 void send() {
-
     bool rslt;
-    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
-        // Always use sizeof() as it gives the size as the number of bytes.
-        // For example if dataToSend was an int sizeof() would correctly return 2
+  
+    rslt = radio.write( &direction, sizeof(direction) );
 
     Serial.print("Data Sent ");
-    Serial.print(dataToSend);
+    Serial.print(direction);
     if (rslt) {
         Serial.println("  Acknowledge received");
-        updateMessage();
     }
     else {
         Serial.println("  Tx failed");
@@ -65,12 +52,3 @@ void send() {
 }
 
 //================
-
-void updateMessage() {
-        // so you can see that new data is being sent
-    txNum += 1;
-    if (txNum > '9') {
-        txNum = '0';
-    }
-    dataToSend[8] = txNum;
-}
